ec6d48090b81f0c088d9f2f52f10800b7ab9f81a
Vishwajeet Bharti <vishwajeetbharti@hotmail.com>
2022-02-05 21:59:59 +0530

    String Interpolation

--- dart_application_1/bin/Data_Type.dart
+++ dart_application_1/bin/Data_Type.dart
@@ -8,6 +8,9 @@ void main(){
   var name1 = "Bharti";
   print(name);
   print(name1);
+  //OR
+  print("My name is $name");
+  print("Length of name is ${name.length+name1.length}");
   // Number: Double.
   double value = 4.66666;
   var value1 = 6.78;
