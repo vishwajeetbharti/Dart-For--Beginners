844d07efc9f80231da168b2dfde007194be58377
Vishwajeet Bharti <vishwajeetbharti@hotmail.com>
2022-02-05 16:20:46 +0530

    Data Type

---
+++ dart_application_1/bin/Data_Type.dart
@@ -0,0 +1,21 @@
+void main(){
+  //It is inferred as integer automatically by compiler.
+  int age1=22;
+  var age=22;
+  print(age/age);
+  // String.
+  String name = "Vishwajeet";
+  var name1 = "Bharti";
+  print(name);
+  print(name1);
+  // Number: Double.
+  double value = 4.66666;
+  var value1 = 6.78;
+  double exponents = 2.3e5;
+  print(value1);
+  print(exponents);
+  // Boolean
+  bool isValid =false;
+  var Istrue= true;
+  print(Istrue);
+}
