bda230fdb4d105b296a23e98010e7238bbbd852b
Vishwajeet Bharti <vishwajeetbharti@hotmail.com>
2022-02-05 15:30:04 +0530

    New Dart Code

---
+++ dart_application_1/bin/dart_application_1.dart
@@ -0,0 +1,9 @@
+void main(){
+  //this simple line using print.
+  print("Hello Vishwajeet.");
+  print("This is my first program in dart.\n");
+  //Arthemetic Operation.
+  print(9*9);
+  //bolean Expression
+  print(true);
+}
